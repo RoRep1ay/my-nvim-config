9ba5d33c8fce63d9aa6cdbe55ffb1f38f9f1d041
Sojimmy Nea <sojimmy.nea@okoone.com>
2022-12-26 10:12:30 +0700

    Restructure Vim config

---
+++ lua/vim-config.lua
@@ -0,0 +1,39 @@
+-- Set highlight on search
+vim.o.hlsearch = true
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+-- Decrease update time
+-- vim.o.updatetime = 250
+-- vim.wo.signcolumn = 'yes'
+
+-- Set colorscheme
+vim.o.termguicolors = true
+vim.cmd [[colorscheme nightfly]]
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+vim.o.scrolloff = 8
+
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldenable = false -- Disable folding at startup
+
